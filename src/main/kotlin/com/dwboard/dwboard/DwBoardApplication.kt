58ae7ca106487360442fb3b4f35452925866ee47
dark2138 <k12u2138@gmail.com>
2025-07-01 19:53:34 +0900

    feat : add first commit

---
+++ src/main/kotlin/com/dwboard/dwboard/DwBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.dwboard.dwboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DwBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<DwBoardApplication>(*args)
+}
